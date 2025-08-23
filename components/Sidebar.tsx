0ae2b806be27fbdc46285ce4ceff6563af1eb79e
gandolfslayer <ashiamurg96@gmail.com>
2025-08-23 04:54:31 -0400

    Rename Inclusivity01 dashboard to Adaptive Nexus and update all references

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            <h1>Adaptive Nexus</h1>
+            {/* Other sidebar content */}
+        </div>
+    );
+};
+
+export default Sidebar;
